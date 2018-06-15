eb0c8b45fb00a42c607511addfa7ce243c26083a
Devarala Prasad <devarala.prasad@kdi.kongsberg.com>
2018-06-15 18:08:02 +0530

    Removed unnecessary folder

---
+++ IoT Samples/SensorData.cs
@@ -0,0 +1,28 @@
+﻿using Kognifai.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M2MqttExampleClient
+{
+    public class SensorData
+    {
+
+        public string ExternalId { get; set; }
+
+        public string TargetNodeDisplayName { get; set; }
+
+        public ulong? ExternalPollFrequency { get; set; }
+
+        public string TargetPath { get; set; }
+        public SensorReadingType ExternalReadingType { get; set; }
+        public ulong? ExternalIndexDelayTimeSec { get; set; }
+        public string ExternalSensorColumnDescription { get; set; }
+        public string ExternalColumnMeasureunit { get; set; }
+        public string TargetColumnMeasureUnit { get; set; }
+
+
+    }
+}
